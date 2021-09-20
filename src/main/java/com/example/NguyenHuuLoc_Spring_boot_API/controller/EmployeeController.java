2d6e65d88a2b5035d3b0c595317f3d93cd894657
Loc DEV <locdev2000@gmail.com>
2021-09-20 21:26:50 +0700

    add profile for dev, production

--- src/main/java/com/example/NguyenHuuLoc_Spring_boot_API/controller/EmployeeController.java
+++ src/main/java/com/example/NguyenHuuLoc_Spring_boot_API/controller/EmployeeController.java
@@ -3,14 +3,9 @@ package com.example.NguyenHuuLoc_Spring_boot_API.controller;
 import com.example.NguyenHuuLoc_Spring_boot_API.dto.EmployeeDTO;
 import com.example.NguyenHuuLoc_Spring_boot_API.entity.Employee;
 import com.example.NguyenHuuLoc_Spring_boot_API.service.EmployeeService;
-import com.example.NguyenHuuLoc_Spring_boot_API.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,15 +15,10 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-
     @GetMapping("/employees")
-    public List<EmployeeDTO> getListEmployee(){
-        List<Employee> employees = employeeService.findAll();
-        List<EmployeeDTO> dtoList = new ArrayList<>();
-        for(Employee e: employees){
-            dtoList.add(new EmployeeDTO(e));
-        }
-        return dtoList;
+    public List<Employee> getListEmployee(){
+        System.out.println(employeeService.findAll());
+        return employeeService.findAll();
     }
 
     @GetMapping("/employees/{id}")
@@ -37,7 +27,7 @@ public class EmployeeController {
     }
 
     @PostMapping("/employees")
-    public Employee addNewEmployee(@Valid @RequestBody Employee employee){
+    public Employee addNewEmployee(@RequestBody Employee employee){
         employee.setId(Long.valueOf(0));
         employeeService.saveEmployee(employee);
         return employee;
